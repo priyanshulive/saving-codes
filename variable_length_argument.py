80a69459be61fccc336d46201015507cb9e796d3
Priyanshu <priyanshurajmicrosoft@gmail.com>
2022-03-11 07:47:57 +0530

    adding some comments

--- variable_length_argument.py
+++ variable_length_argument.py
@@ -1,7 +1,7 @@
-def add (*P):
+def add (*P):       #In P we can pass arguments according to its need
     print(P)
     sum=0
-    for x in P:
+    for x in P:   
         sum = sum + x
     print(sum)
 add(3,4,5)
