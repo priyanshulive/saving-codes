e3c26df8cb5b895fee0a7650675d4206821f89a3
Priyanshu <priyanshurajmicrosoft@gmail.com>
2022-03-18 21:36:55 +0530

    In this file i will save the string built-in functions

---
+++ string functions/replace_function.py
@@ -0,0 +1,6 @@
+#print("today is a great day".replace("is","iss"))
+n=input("enter the string:  ")
+m=input("enter the string you want to replace:  ")
+o=input ("enter the new string:  ")
+print(n.replace(m,o))
+# with replace function you can change a word of the sentence
---
+++ string functions/split_function.py
@@ -0,0 +1,3 @@
+# print("today is great".split())
+a=input("enter string you want to split:  ")
+print(a.split())
