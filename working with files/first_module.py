d44e49c4c561e06a70fe205b81a02e53d570a249
Priyanshu <priyanshurajmicrosoft@gmail.com>
2022-03-18 20:21:55 +0530

    how to import program and function from one file to another

---
+++ working with files/first_module.py
@@ -0,0 +1,6 @@
+pi=3.14
+def area_of_circle(r):
+    return pi*r*r
+def add(x,y):
+    p=x+y
+    return p 
---
+++ working with files/how_to_import_module.py
@@ -0,0 +1,3 @@
+import first_module
+print(first_module.area_of_circle(5))
+print(first_module.add(7,8))
