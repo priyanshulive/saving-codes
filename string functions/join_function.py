301e986cb578d0b0c3182d77e934bd5a07aafbdc
Priyanshu <priyanshurajmicrosoft@gmail.com>
2022-03-18 21:00:01 +0530

    include join function and its use with example

---
+++ string functions/join_function.py
@@ -0,0 +1,5 @@
+n=input("enter string: ")
+m=input("what you want to join: ")
+print(m.join(n))
+# print("abc".join("hello world"))
+#join function add symbols between every character i.e. habceabclabclabc
