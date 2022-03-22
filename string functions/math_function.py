31fc3eb9cde982a76822de3373d91b740f7ec972
Priyanshu <priyanshurajmicrosoft@gmail.com>
2022-03-22 14:37:42 +0530

    added comments for future or further development

--- string functions/math_function.py
+++ string functions/math_function.py
@@ -1,8 +1,7 @@
 import math
 print(math.sqrt(4))
 print(math.pow(2,3))
-a=math.floor(42.4)
+a=math.floor(42.4)        #it gives lower value of a integer
 print(a)
-print(math.ceil(43.4))
-print(math.fabs(-5))
-print(math.sin(4))
+print(math.ceil(43.4))    #it gives higher value of a integer
+print(math.fabs(-5))      #it gives absolute value means only positive values
